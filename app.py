6dcd45b74bfaddd582ccf7e050ed441288ad3fb2
Parashar Dhapola <parashar.dhapola@gmail.com>
2018-12-07 02:23:59 +0100

    Complete rewrite. Included D3

--- app.py
+++ app.py
@@ -7,92 +7,55 @@ from matplotlib import colors as mcolors
 import h5py
 import re
 
-named_colors = {k.capitalize():v for k,v in
-                dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS).items() if len(k) > 1}
-
-def make_fig(dataset, genes, figsize=(6, 6), fs=14, display_std=True,
-             lw=1, lc='crimson', fc='crimson'):
-    
-    def read_data(fn, genes):
-        a = h5py.File(fn, mode='r', swmr=True)
-        data_mean = [np.array([x.decode('UTF-8') for x in a['data']['celltypes'][:]])]
-        data_std = [data_mean[0]]
-        index = ['cells']
-        for i in genes:
-            try:
-                x = a['data'][i][:]
-            except KeyError:
-                continue
-            else:
-                data_mean.append(x[0])
-                data_std.append(x[1])
-                index.append(i)
-        return (pd.DataFrame(data_mean, index=index).T.set_index('cells'),
-                pd.DataFrame(data_std, index=index).T.set_index('cells'))
+def prep_data(dataset, genes):
 
     def normalize(a):
         a = a-a.min()
         a = a/a.max()
         return list(a) + [a[0]]
 
-    def plot_vals(ax, mean, std, show_std, line_color, fill_color, line_width):
-        y1 = normalize(mean.median(axis=1))
-        y2 = normalize((mean - std).median(axis=1))
-        y3 = normalize((mean + std).median(axis=1))
-        pos = np.radians(np.linspace(0, 360, len(mean)+1, endpoint=True))
-        if show_std:
-            for i in [y1, y2, y3]:
-                ax.plot(pos, i, lw=line_width, c=line_color)
-                ax.fill_between(pos, 0, i, alpha=0.3, color=fill_color)
-        else:
-            ax.plot(pos, y1, lw=line_width, c=line_color)
-            ax.fill_between(pos, 0, y1, alpha=0.3, color=fill_color)
-        ax.set_xticks(pos)
+    blank_return = {'xvals': [], 'ylist': [], 'genes': [], 'cells': []}
 
-    def make_labels(ax, labels, label_size):
-        coords = np.radians(np.linspace(0, 360, len(labels)+1, endpoint=True))[:-1]
-        for coord, label in zip(coords, labels):
-            d = np.rad2deg(coord)
-            if d < 90 or d > 270:
-                rotation = d
-            else:
-                rotation = 180 + d
-            ax.text(coord, 1.3, label.replace('_', '\n'), rotation=rotation,
-                        va='center', ha='center', fontsize=label_size, zorder=0)
-
-    def make_svg(figure):
-        imgdata = StringIO()
-        fig.savefig(imgdata, format='svg')
-        imgdata.seek(0)
-        retval = imgdata.read()
-        imgdata.close()
-        return retval
-
-    def clean_axis(ax):
-        ax.grid(which='major', linestyle='--', alpha=0.4)
-        ax.set_facecolor('white')
-        ax.figure.patch.set_facecolor('white')
-        ax.set_yticks([])
-        ax.set_xticklabels([''])
-        ax.set_yticklabels([''])
-        ax.spines["polar"].set_visible(False)
-        ax.figure.patch.set_alpha(0)
-        ax.patch.set_alpha(0)
-        ax.set_ylim((0, 1.5))
+    if len(genes) == 0:
+        return {**blank_return, **{'msg':
+         'Please enter atleast one gene name'}}
+    if dataset not in DATAFILES:
+        return {**blank_return, **{'msg':
+         'Selected datatset is invalid'}}
+    genes = [re.sub('[^0-9a-zA-Z]+-', '', x).upper() for x in genes]
     
-    fig = plt.figure(figsize=figsize)
-    ax = fig.add_subplot(111, polar=True)
-    mean, std = read_data()
-    if len(mean.columns) == 0:
-        return '', []
-    plot_vals(ax, mean, std, display_std, lc, fc, lw)
-    make_labels(ax, list(mean.index), fs)
-    clean_axis(ax)
-    plt.tight_layout()
-    retval = make_svg(fig)
-    fig.clear()
-    plt.close()
-    return retval, list(mean.columns)
+    h5fn = h5py.File(DATAFILES[dataset], mode='r', swmr=True)
+    data_mean = [np.array([x.decode('UTF-8') for x
+                 in h5fn['data']['celltypes'][:]])]
+    data_std = [data_mean[0]]
+    index = {'cells': None}
+    for i in genes:
+        if i in index:
+            continue
+        try:
+            x = h5fn['data'][i][:]
+        except KeyError:
+            continue
+        else:
+            data_mean.append(x[0])
+            data_std.append(x[1])
+            index[i] = None
+    h5fn.close()
+    index = list(index.keys())
+    if len(index) == 1:
+        return {**blank_return, **{'msg':
+         'None of the entered gene names is valid'}}
+    mean = pd.DataFrame(data_mean, index=index).T.set_index('cells')
+    std = pd.DataFrame(data_std, index=index).T.set_index('cells')
+    y1 = normalize(mean.median(axis=1))
+    y2 = normalize((mean - std).median(axis=1))
+    y3 = normalize((mean + std).median(axis=1))
+    x = list(np.radians(np.linspace(0, 360, len(mean)+1, endpoint=True)))
+    return {
+        'xvals': x, 'ylist': [y1, y2, y3],
+        'genes': '\n'.join(list(mean.columns)), 'cells': list(mean.index),
+        'msg': 'OK'
+    }
 
 DATAFILES = {
     'Mouse normal hematopoiesis (BloodSpot)': 'datasets/mouse_normal_hematopoiesis_bloodspot.h5',
@@ -109,44 +72,19 @@ else:
 @app.route("%s" % url_prefix)
 def index():
     return render_template('index.html',
-     randstr='?rand%d' % np.random.randint(100000000000000))
+     randstr='?rand%d' % np.random.randint(1e8))
 
 @app.route("%s/getdatasets" % url_prefix, methods=['GET'])
 def get_datasets():
     return jsonify({
-        'datasets': list(DATAFILES.keys()),
+        'datasets': [{'id': 'dataset%d' % n, 'value': x } for n,x in
+                     enumerate(DATAFILES.keys(), 1)],
     })
 
 @app.route("%s/makeradar" % url_prefix, methods=['POST'])
 def make_radar():
     data = request.get_json()
-    genes = [re.sub('[^0-9a-zA-Z]+', '', x).upper() for x in data['genes']]
-    if data['dataset'] not in DATAFILES:
-        return jsonify({
-            'valid_genes': '\n'.join(genes),
-            'svg': '%s not present in the database' % data['dataset']
-        })
-    else:
-        svg, valid_genes = make_fig(
-            data['dataset'], genes, figsize=(6, 6), display_std=True,
-            lw=1, fs=int(data['fontSize']),
-            lc=data['lineColor'].lower(), fc=data['fillColor'].lower())
-        if len(valid_genes) == 0:
-            return jsonify({
-                'valid_genes': '',
-                'svg': 'Atleast one valid gene required'
-            })
-        else:
-            return jsonify({
-                'valid_genes': '\n'.join(valid_genes),
-                'svg': svg
-            })
-
-@app.route("/mpl_colors", methods=['GET'])
-def mpl_colors():
-    return jsonify({
-        'mpl_colors': named_colors
-    })
+    return jsonify(prep_data(data['dataset'], data['genes']))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- static/js/index.jsx
+++ static/js/index.jsx
@@ -1,258 +1,166 @@
-class GeneBox extends React.Component {
-	render () {
-		return ()
-	}
-}
-
-class SvgDisplay extends React.Component {
-	render () {
-		return ()
-	}
-}
-
-class SvgCustomize extends React.Component {
-	render () {
-		return ()
-	}
-}
-
 class DatasetSelect extends React.Component {
-	render () {
-		return ()
-	}
-}
-
-
-
-
-
-
-
-
-
-class GeneBox extends React.Component {
 	constructor () {
 		super()
-		this.updateGeneList	= this.updateGeneList.bind(this)
-		this.updateLineColor = this.updateLineColor.bind(this)
-		this.updateFillColor = this.updateFillColor.bind(this)
-		this.updateFontSize = this.updateFontSize.bind(this)
-		this.state = {
-			svgData: '', mpl_colors: {},
-			disableSubmitButton: false, showDownloadLink: false,
-			fontSize: 14, fillColor: 'crimson', lineColor: 'crimson',
-			 
-		};
+		this.state = {'datasets': []}
 	}
-
-	componentDidMount (){
-		document.getElementById(this.props.uid).value = 'MPO\nELANE\nFLT3\nEGFL7';
-		this.updateGeneList()
-		fetch('/cellradar/mpl_colors')
+	componentWillMount () {
+		console.log('fetching datatset')
+		fetch('/cellradar/getdatasets')
 			.then(response => response.json())
 			.then(r => {
-				this.state['mpl_colors'] = r['mpl_colors'];
-				this.setState(this.state);
+				this.setState({'datasets': r['datasets']})
+				this.props.callBackFunc(r['datasets'][0]['value'])
 			})
 	}
+	render () {
+		console.log('Rendering DatasetSelect')
+		return (
+			<div>
+				<b>Select dataset: </b>
+				<select style={{'width':'250px', 'font-size':'12px'}}
+					onInput={e => this.props.callBackFunc(e.target.value)}>
+					{this.state.datasets.map(item => (
+						<option key={item.id}>{item.value}</option>
+					))}
+				</select>
+			</div>
+		)
+	}
+}
 
-	componentDidUpdate () {
-		document.getElementById(this.props.svguid).innerHTML = this.state.svgData;
+class GeneBox extends React.Component {
+	onTextUpdate(v) {
+		this.props.callBackFunc(v.split("\n").filter(g => g.length > 0))
+	}
+	render () {
+		console.log('Rendering GeneBox')
+		return (
+			<div>
+				<p>
+					<b>Paste gene symbols below</b><br />
+			  		<small>(One gene per line)</small>
+				</p>
+				<textarea rows="10" cols="30" id={this.props.uid} 
+						  onInput={e => this.onTextUpdate(e.target.value)} />
+				<br />
+				<small><i>Invalid gene names will be removed</i></small>
+				<br />
+			</div>
+		)
+	}
+}
 
-		if (this.state.showDownloadLink) {
-			var textToSaveAsBlob = new Blob([this.state.svgData],
-											{type:"text/plain"});
-	    	document.getElementById(this.props.downloaduid).href = 
-	    		window.URL.createObjectURL(textToSaveAsBlob);
+class InputStore extends React.Component {
+	shouldComponentUpdate() {
+		return false
+	}
+	render () {
+		console.log('Rendering InputStore')
+		return (
+			<div>
+				<DatasetSelect
+					callBackFunc={v => this.props.callBackFunc(v, 'selectedDataset')} />
+				<br />
+				<GeneBox uid={this.props.uid}
+					callBackFunc={v => this.props.callBackFunc(v, 'inputGenes')} />
+			</div>
+		)
+	}
+}
+
+class SubmitButton extends React.Component {
+	render() {
+		console.log('Rendering SubmitButton')
+		return (
+			<div>
+				{this.props.isDisabled ?
+					<button className="btn" disabled>Wait</button>:
+					<button className="btn"
+							onClick={this.props.callBackFunc}>Submit</button>
+				}
+			</div>
+		)	
+	}
+}
+
+class App extends React.Component {
+	constructor () {
+		super()
+		this.state = {
+			'geneBoxid': 'geneBox1', 'selectedDataset': '', 'inputGenes': [],
+			'SubmitButtonDisabled': false, 'radarData': {}
 		}
 	}
-
-	updateLineColor (e) {
-		this.state.lineColor = e.target.value;
-		this.updateGeneList()
-	}
-
-	updateFillColor (e) {
-		this.state.fillColor = e.target.value;
-		this.updateGeneList()
-	}
-
-	updateFontSize (e) {
-		this.state.fontSize = e.target.value;
-		this.updateGeneList()
-	}
-
-	updateGeneList () {
-		const geneList = document.getElementById(this.props.uid)
-						.value.split("\n").filter(g => g.length > 0);
+	fetchRadarData () {
+		console.log('Fetching radarData')
+		this.setState({'SubmitButtonDisabled': true})
 		const req_data = {
 			method: "POST",
 			headers: {
             	"Content-Type": "application/json; charset=utf-8",
         	},
 			body: JSON.stringify({
-				'genes': geneList,
-				'dataset': this.props.dataset,
-				'fillColor': this.state.fillColor,
-				'lineColor': this.state.lineColor,
-				'fontSize': this.state.fontSize
+				'dataset': this.state.selectedDataset,
+			  	'genes': this.state.inputGenes
 			})		
 		}
-		this.state.disableSubmitButton = true;
-		this.state.svgData = '';
-		this.state.showDownloadLink = false;
-		this.setState(this.state);
-
 		fetch('/cellradar/makeradar', req_data)
-		.then(response => response.json()).then(res => {
-			var showDownloadLink = true
-			if (res['svg'].length < 100) {
-				showDownloadLink = false
-			}
-			document.getElementById(this.props.uid).value = res['valid_genes'];
-			this.state.disableSubmitButton = false;
-			this.state.svgData = res['svg'];
-			this.state.showDownloadLink = showDownloadLink;
-			this.setState(this.state);
-		})
+			.then(response => response.json())
+			.then(r => {
+				this.setState({'SubmitButtonDisabled': false, 'radarData': r})
+				document.getElementById(this.state.geneBoxid).value = r['genes']
+				console.log(this.state)
+                this.makeradar()
+			})
 	}
+    makeradar = () => {
+        console.log ('Rendering RadarD3')
+        var data = []
+        for (var i in this.state.radarData.cells) {
+            data.push({'axis': this.state.radarData.cells[i],
+                       'value': this.state.radarData.ylist[0][i]})
+        }
+
+        var margin = {top: 60, right: 60, bottom: 60, left: 60}
+        var width = Math.min(700, window.innerWidth - 10) - margin.left - margin.right
+        var height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20)
+        var width = 350
+        var height = 350
+        var color = d3.scale.ordinal()
+            .range(["#EDC951"]);
+            
+        var radarChartOptions = {
+          w: width,
+          h: height,
+          margin: margin,
+          maxValue: 0.5,
+          levels: 5,
+          roundStrokes: true,
+          color: color
+        };
+        RadarChart(".radarChart", [data], radarChartOptions);
+    }   
 
 	render () {
-
-		var color_options = [];
-		for (var i in this.state.mpl_colors) {
-			color_options.push(<option style={{'background-color': this.state.mpl_colors[i]}}
-								       value={i}>{i}</option>)
-		}
-
-		return (
-			<div>
-
-				<div className="row">
-					<div className="col-4">
-						<br />
-						<p>
-							<b>Paste gene symbols below</b><br />
-						  	<small>(One gene per line)</small>
-						</p>
-						<textarea rows="20" cols="20" id={this.props.uid} />
-						<br />
-						{this.state.disableSubmitButton ? 
-							<button className="btn" disabled>Wait</button>:
-							<button className="btn"
-								    onClick={this.updateGeneList}>Submit</button>
-						}
-						<br />
-						<small>Invalid gene names will be removed</small>
-						<br />
-					</div>
-					
-					<div className="col-8">
-						<br />
-						<div id={this.props.svguid}></div>
-						{this.state.showDownloadLink ? 
-							<a id={this.props.downloaduid} href=''
-							   download='CellRadar.svg'>Download SVG</a>:
-							<p></p>
-						}				
-					</div>
-
-	    		</div>
-
-	    		<div className="row">
-	    			<div className="col-4"></div>
-	    			<div className="col-8">
-		    			<div className="row">
-							<div className="col-3">Fill color</div>
-							<div className="col-9">
-								{this.state.disableSubmitButton ?
-									<select onInput={this.updateFillColor} disabled>
-										{color_options}
-									</select>:
-									<select onInput={this.updateFillColor}>
-										{color_options}
-									</select>
-								}
-							</div>
-						</div>
-						<div className="row">
-							<div className="col-3">Line color</div>
-							<div className="col-9">
-								{this.state.disableSubmitButton ?
-									<select disabled>
-										{color_options}
-									</select>:
-									<select onInput={this.updateLineColor}>
-										{color_options}
-									</select>
-								}
-							</div>
-						</div>
-						<div className="row">
-							<div className="col-3">Font size</div>
-							<div className="col-9">
-								{this.state.disableSubmitButton ?
-									<input type="range" min="4" max="36" step="2"
-										   value={this.state.fontSize} disabled/>
-									:
-									<input type="range" min="4" max="36" step="2"
-										   value={this.state.fontSize}
-										   onChange={this.updateFontSize}/>
-								}
-							</div>
-						</div>
-					</div>
-				</div>
-			
-			</div>
-		)
-	}
-}
-
-
-class App extends React.Component {
-	constructor () {
-		super()
-		this.updateDataset	= this.updateDataset.bind(this)
-		this.state = {dataset: 'mouse_normal_bloodspot'};
-	}
-
-	updateDataset (e) {
-		this.setState({dataset: e.target.value});
-	}
-
-	render () {
+		console.log('Rendering App')
 		return (
 			<div className="container">
 				<br />
-
-				<div className="row">
-					<div className="col-1"></div>
-
-					<div className="col-10">
-						<h1>Welcome to CellRadar</h1>
-						
-						<div className="row">
-							<div className="col-3">
-								<br />
-								<p>Select dataset:</p>
-							</div>
-							<div className="col-9">
-								<br />
-								<select onInput={this.updateDataset}>
-						 			<option value="mouse_normal_bloodspot">
-						 				Mouse normal hematopoiesis</option>
-						 			<option value="human">Human</option>
-								</select>
-							</div>
-						</div>
-
-						<GeneBox downloaduid='downsvg' svguid='svgimage' uid='genetext'
-								 dataset={this.state.dataset} />
+				<h2>Welcome to CellRadar</h2>
+				<br />
+				<div className='row'>
+					<div className='col-4'>
+						<InputStore uid={this.state.geneBoxid}
+							callBackFunc={(v, s) => {this.state[s] = v}} />
+						<SubmitButton isDisabled={this.state.SubmitButtonDisabled}
+							callBackFunc={() => this.fetchRadarData()} />
 					</div>
-
-					<div className="col-1"></div>
+					<div className='col-6'>
+                        <div className='radarChart'>
+                        </div>
+					</div>
 				</div>
+				<br />
 			</div>
 		)
 	}
